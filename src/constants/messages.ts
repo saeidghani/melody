a5d4ceec0af403eb901fbf468d727ac4d48df62c
Saeid Ghani <saeidghani@gmail.com>
2023-08-16 08:50:50 +0330

    add constants

---
+++ src/constants/messages.ts
@@ -0,0 +1,12 @@
+export const messages = {
+  email: {
+    required: "Please enter your email",
+    invalid: "Please enter a valid email",
+  },
+  password: {
+    required: "Please enter your password",
+    min: "Please enter a valid password",
+    passwordConfirm: "Please confirm your password",
+    passwordConfirmMisMatch: "Password and password confirm are not equal",
+  },
+};
---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const routes = {
+  home: "/",
+  auth: {
+    signin: "/signin",
+    signup: "/signup",
+  },
+};
