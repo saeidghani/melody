410fc1a3a3cfda7ad8b6e998d5467f17ed64d17c
Saeid Ghani <saeidghani@gmail.com>
2023-08-17 15:17:12 +0330

    add home link to auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,7 @@
 import { Card } from "@/components/ui/card";
+import { Link } from "@/components/ui/link";
+import { routes } from "@/constants";
+import { ChevronLeft } from "lucide-react";
 import React, { ReactNode } from "react";
 
 interface LayoutProps {
@@ -15,6 +18,10 @@ export default function layout({ children }: LayoutProps) {
         className="w-full max-w-[400px] mx-auto max-xs:border-none 
                    max-xs:shadow-none max-xs:rounded-none p-4 md:p-6"
       >
+        <Link size="xs" className="mr-auto" href={routes.home}>
+          <ChevronLeft size={16} />
+          Home
+        </Link>
         {children}
       </Card>
     </main>
